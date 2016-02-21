dab1a2e94c3c165c18d957a496055f723e07a25e
Kha Nguyen <kha@tinypulse.com>
2016-02-21 21:01:47 +0700

    change to button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -75,6 +75,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    @article.comments.delete_all
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
