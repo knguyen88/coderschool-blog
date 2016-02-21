d0cad318a2a111b5c74bfb71069141e0bef52fcc
Kha Nguyen <kha@tinypulse.com>
2016-02-21 16:12:47 +0700

    add views count

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,22 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :initialize_markdown, only: [:show, :index]
 
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    if params[:searchKeyword].blank?
+      @articles = Article.all
+    else
+      @articles = Article.where('title LIKE ?', "%#{params[:searchKeyword]}%")
+    end
   end
 
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @article.views = @article.views + 1
+    @article.save
   end
 
   # GET /articles/new
@@ -63,13 +69,17 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def initialize_markdown
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  end
 end
